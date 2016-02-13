d897af0d164c6955784bef9eee91bd47920b9865
alireza <pourranjbar.ar@gmail.com>
2016-02-13 20:13:11 +0330

    song image added

--- rbt/models.py
+++ rbt/models.py
@@ -19,6 +19,8 @@ class Song(models.Model):
     rate = models.IntegerField(default=0)
     activated = models.IntegerField(default=0)
     producer = models.ForeignKey(Producer,null=True,blank=True)
+    image = models.ImageField(upload_to='images/',null=True, blank=True)
+
 
     def __unicode__(self):
         return self.song_name + " -- " + self.producer.__unicode__()
