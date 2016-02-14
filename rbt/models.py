59239b0a8c14a32447e19b34c75018c26cb02ad7
alireza <pourranjbar.ar@gmail.com>
2016-02-14 15:42:47 +0330

    media file locations

--- rbt/models.py
+++ rbt/models.py
@@ -25,10 +25,10 @@ class Song(models.Model):
     rate = models.IntegerField(default=0)
     activated = models.IntegerField(default=0)
     producer = models.ForeignKey(Producer,null=True,blank=True)
-    image = models.ImageField(upload_to='images/',null=True, blank=True)
+    image = models.ImageField(upload_to='',null=True, blank=True)
     category = models.ForeignKey(Category)
     price = models.IntegerField(default=0)
-    
+
 
     def __unicode__(self):
         return self.song_name + " -- " + self.producer.__unicode__()
