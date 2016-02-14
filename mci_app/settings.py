8936ea3eb1e4b771038d2dd0d411d8d48070b2b4
alireza <pourranjbar.ar@gmail.com>
2016-02-14 15:47:25 +0330

    settings updated

--- mci_app/settings.py
+++ mci_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9atti-7p6u&e934o(+wzhn(ds#j5&lb2!%kcnk+4m(ogzpvee^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
