ab6807efdec1edacd6fe61c34be218dd1074f70b
alireza <pourranjbar.ar@gmail.com>
2016-03-30 12:32:48 +0430

    pop music producer

---
+++ rbt/populate_producer_pop_music.py
@@ -0,0 +1,14 @@
+from models import Album, Song, Category
+
+
+def populate_producer(album):
+
+    if album.producer == None:
+        first_song = Song.objects.filter(album=album)[0]
+        album.producer = first_song.producer
+        album.save()
+
+def populate_producer_pop_songs():
+
+    for album in Album.objects.filter(category__english_name='pop'):
+        populate_producer(album)
