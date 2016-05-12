a6b68b208ea1ea4b61f6edab30a9343ed114309e
alireza <pourranjbar.ar@gmail.com>
2016-05-12 20:15:18 +0430

    utilities

---
+++ rbt/utilities.py
@@ -0,0 +1,13 @@
+from models import Album, Song
+
+def move_songs(start,target,number_of_songs):
+
+    sAlbum = Album.objects.get(id=start)
+    tAlbum = Album.objects.get(id=target)
+
+    songs = Song.objects.filter(album = sAlbum)[0:number_of_songs]
+
+    for song in songs:
+
+        song.album = tAlbum
+        song.save()
