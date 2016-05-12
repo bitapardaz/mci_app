a616ec3d005cca399cf66dff072756be4086f697
alireza <pourranjbar.ar@gmail.com>
2016-05-12 15:48:50 +0430

    minor change

--- rbt/populate_producer_pop_music.py
+++ rbt/populate_producer_pop_music.py
@@ -10,5 +10,5 @@ def populate_producer(album):
 
 def populate_producer_pop_songs():
 
-    for album in Album.objects.filter(category__english_name='pop'):
+    for album in Album.objects.filter(category__english_name='traditional'):
         populate_producer(album)
