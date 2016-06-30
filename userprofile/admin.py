3bd4c00332a094db8b466090b4dd60d28a594d93
alireza <pourranjbar.ar@gmail.com>
2016-06-30 13:58:00 +0430

    fix

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -8,10 +8,9 @@ class ActivationRequestAdmin(admin.ModelAdmin):
 
     model = ActivationRequest
 
-    #list_display = ('user_profile', 'song', 'time_stamp', 'activated', 'WhereAmI')
-    #search_fields = ['user_profile__mobile_number', 'song__song_name']
+    list_display = ('user_profile', 'song', 'time_stamp', 'activated', 'where_am_i')
+    search_fields = ['user_profile__mobile_number', 'song__song_name']
 
 
 admin.site.register(UserProfile)
 admin.site.register(ActivationRequest,ActivationRequestAdmin)
-#admin.site.register(ActivationRequest)
