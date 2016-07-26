2302fe09527d9d63b844be213c70f4160e4e8429
alireza <pourranjbar.ar@gmail.com>
2016-07-26 17:54:39 +0430

    fix

--- tci/utilities.py
+++ tci/utilities.py
@@ -67,83 +67,114 @@ class SafeString(str):
     def capitalize(self):
         return self
 
+def pay():
+    mobile_no = "09125498004"
+    print "mobile_no:%s" % mobile_no
 
-mobile_no = "09125498004"
-print "mobile_no:%s" % mobile_no
+    bill_id = "4445152300146"
+    print "bill_id:%s" % bill_id
 
-bill_id = "4445152300146"
-print "bill_id:%s" % bill_id
+    pay_id = "150166"
+    print "pay_id:%s" % pay_id
 
-pay_id = "150166"
-print "pay_id:%s" % pay_id
+    pan = "6219861024245069"
+    print "pan:%s" % pan
 
-pan = "6219861024245069"
-print "pan:%s" % pan
-
-pin2 = "14725"
-print "pin2:%s" % pin2
-
-
-pec_request = {}
-pec_request['MobileNo'] = mobile_no
-pec_request['PayInfo'] = generate_pay_info(pan,pin2)
-pec_request['Token'] = '0'
-pec_request['BillId'] = bill_id
-pec_request['PayId'] = pay_id
-pec_request['TerminalPin'] = "84y80M17HW810Y2j0434"
-
-data = json.dumps(pec_request)
-print "-------------------"
-print "json data sent to pec:"
-print "-------------------"
-print data
-print "----------------------------------"
+    pin2 = "14725"
+    print "pin2:%s" % pin2
 
 
+    pec_request = {}
+    pec_request['MobileNo'] = mobile_no
+    pec_request['PayInfo'] = generate_pay_info(pan,pin2)
+    pec_request['Token'] = '0'
+    pec_request['BillId'] = bill_id
+    pec_request['PayId'] = pay_id
+    pec_request['TerminalPin'] = "84y80M17HW810Y2j0434"
 
-print "-------------------------------------"
-# Processing payment using pec_request
-print "Processing Payment Step"
-print "-------------------------------------"
+    data = json.dumps(pec_request)
+    print "-------------------"
+    print "json data sent to pec:"
+    print "-------------------"
+    print data
+    print "----------------------------------"
 
-#url = "https://app.pec.ir/api/Payment/BillPaymentGeneral"
-url = "https://testapp.pec.ir/api/Payment/BillPaymentGeneral"
-username = 'Pishahang'
-password = 'P!$h@h@ng0502'
+    print "-------------------------------------"
+    # Processing payment using pec_request
+    print "Processing Payment Step"
+    print "-------------------------------------"
+
+    #url = "https://app.pec.ir/api/Payment/BillPaymentGeneral"
+    url = "https://testapp.pec.ir/api/Payment/BillPaymentGeneral"
+    username = 'Pishahang'
+    password = 'P!$h@h@ng0502'
+
+    request = urllib2.Request(url)
+    base64string = base64.encodestring('%s|%s' % (username, password)).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)
+    request.add_header("Content-Type","application/json")
+    request.add_header(SafeString("appVersion"),"1.7")
+
+    print "-------------------"
+    print "Header Items"
+    print "-------------------"
+    print request.header_items()
+
+
+    result = urllib2.urlopen(request,data)
+    print "---------------------"
+    print "result received from the server:"
+    print result
+    print "---------------------"
+
+    j_result = json.loads(result.read().strip())
+    status = j_result["Status"]
+    message = j_result["Message"]
+    Data = j_result["Data"]
+
+    print "--------------------"
+    print "Result turned into the format"
+    print j_result
+    print "--------------------"
+
+    # Client Response
+    client_response  = {}
+    client_response['Status'] = status
+    client_response['Message'] = message
+
+    # turn bill_info into json and return
+    response =  Response(client_response)
+    print "Done"
+
+
+####################################################
+### confirm payment
+####################################################
+
+url = 'https://Services.pec.ir/api/Telecom/Bill/PayInfoRequest'
+username = 'aryan'
+password = '123123@'
 
 request = urllib2.Request(url)
-base64string = base64.encodestring('%s|%s' % (username, password)).replace('\n', '')
+base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
 request.add_header("Authorization", "Basic %s" % base64string)
 request.add_header("Content-Type","application/json")
-request.add_header(SafeString("appVersion"),"1.7")
 
-print "-------------------"
-print "Header Items"
-print "-------------------"
-print request.header_items()
+data = {}
+data['BillId'] = "4445152300146"
+data['PayId'] = "150166"
+data['RRN'] = "703529447612"
 
+j_data = json.dumps(data)
+result = urllib2.urlopen(request,j_data)
 
-result = urllib2.urlopen(request,data)
-print "---------------------"
-print "result received from the server:"
-print result
-print "---------------------"
+# analysis of the response
+j_response = json.loads(result.read().strip())
 
-j_result = json.loads(result.read().strip())
-status = j_result["Status"]
-message = j_result["Message"]
-Data = j_result["Data"]
+output = {}
+output['status'] = j_response['Status']
+output['message'] = j_response['Message']
 
-print "--------------------"
-print "Result turned into the format"
-print j_result
-print "--------------------"
-
-# Client Response
-client_response  = {}
-client_response['Status'] = status
-client_response['Message'] = message
-
-# turn bill_info into json and return
-response =  Response(client_response)
-print "Done"
+print "payment confirmation - output status: %s" % output['status']
+print "payment confirmation - output message: %s" % output['message']
+return output
