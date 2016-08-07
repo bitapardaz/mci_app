04e9ec7a79f925094b55682a1a33a9f81c40cdfd
alireza <pourranjbar.ar@gmail.com>
2016-08-07 12:35:28 +0430

    test

--- tci/models.py
+++ tci/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Phone(models.Model):
     tel_no = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.tel_no
