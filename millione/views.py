1c0d15d811fcefef00171aab7e4716399c2a4eed
alireza <pourranjbar.ar@gmail.com>
2017-06-11 18:23:06 +0430

    fix

--- millione/views.py
+++ millione/views.py
@@ -16,6 +16,7 @@ def top_up(request,format=None):
     amount = request.data.get('amount')
     mobile_number = request.data.get('mobile_number')
     operator = request.data.get('operator')
+
     output = utilities.top_up_air_time(amount,mobile_number,operator)
     return Response(output)
 
